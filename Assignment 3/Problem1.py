89dba99fc880d70716ca2da5bca97855e0d99c28
Marcus Aurén <auren@student.chalmers.se>
2019-03-06 14:40:28 +0100

    Pretty much done

--- Assignment 3/Problem1.py
+++ Assignment 3/Problem1.py
@@ -15,23 +15,32 @@ G = matrix([[ 2.0,  2.0, -4.0,  4.0,  8.0],
 
 h = matrix([6.0, 1.0, 5.0, -5.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-(stat1, solx, soly) = glpk.lp(c, G.T, h)
-print("Optimal value for Primal:", c.T * solx * -1)
-print("Solution for Primal: \n", solx)
+(stat1, primalx, primaly) = glpk.lp(c, G.T, h)
+print("Optimal value for Primal:", c.T * primalx * -1)
+print("Solution for Primal: \n", primalx)
 
 
-c = matrix([6.0, -1.0, 5.0, 4.0])
-G = matrix([[-2.0, -2.0, -5.0, -2.0],
+c1 = matrix([6.0, -1.0, 5.0, 4.0])
+G1 = matrix([[-2.0, -2.0, -5.0, -2.0],
             [-2.0, -1.0,  2.0,  2.0],
-            [4.0,   2.0, -4.0, -5.0],
+            [ 4.0,  2.0, -4.0, -5.0],
             [-4.0,  1.0, -4.0, -3.0],
             [-8.0,  3.0, -2.0, -1.0],
             [-1.0,  0.0,  0.0,  0.0],
-            [0.0,   1.0,  0.0,  0.0],
-            [0.0,   0.0,  0.0, -1.0]])
+            [ 0.0,  1.0,  0.0,  0.0],
+            [ 0.0,  0.0,  0.0, -1.0]])
 
-h = matrix([-4.0, 2.0, -5.0, -6.0, -7.0, 0.0, 0.0, 0.0])
+h1 = matrix([-4.0, 2.0, -5.0, -6.0, -7.0, 0.0, 0.0, 0.0])
 
-(stat1, solx, soly) = glpk.lp(c, G.T, h)
-print("Optimal value for Dual:", c.T * solx)
-print("Solution for Dual: \n", solx)
+(stat1, dualx, dualy) = glpk.lp(c1, G1.T, h1)
+print("Optimal value for Dual:", c1.T * dualx)
+print("Solution for Dual: \n", dualx)
+
+print("Complementary Slackness for x")
+for i in range(5):
+    print((G1[:, i].T * dualx - h1[i]) * primalx[i])
+
+
+print("Complementary Slackness for y")
+for j in range(4):
+    print((G[:, j].T * primalx - h[j]) * dualx[j])
--- Assignment 3/Problem3.py
+++ Assignment 3/Problem3.py
@@ -1,21 +1,27 @@
-from cvxopt import glpk, matrix, solvers
 import numpy as np
 
 G = np.genfromtxt("graph.txt")
 
 A = np.zeros((200, 200))
 chosenNodes = np.zeros(200)
+emptyVec = np.zeros(200)
 
-for i in range(100000):
-    if np.sum(G) == 0:
-        break
+while np.sum(G):
     n = 1/1000
     A = A + G*n
     sums = np.sum(A, axis=1)
     for val in range(sums.size):
-        if sums[val] > 1:
-            G[val] = np.zeros(200)
-            G[:, val] = np.zeros(200)
-            chosenNodes[val-1] = 1
+        if sums[val] >= 1:
+            if np.sum(G[val]) != 0:
+                G[val] = emptyVec
+            if np.sum(G[:, val]) != 0:
+                G[:, val] = emptyVec
+            chosenNodes[val] = 1
 
-print(sum(chosenNodes))
+print("The algorithm chose", np.sum(chosenNodes), "nodes")
+
+var = input("Do you want to print the chosen nodes? (y/n)  ")
+if var == 'y':
+    idx = np.where(chosenNodes == 1)
+    idx = [x + 1 for x in idx]
+    print("The chosen nodes are: \n", np.concatenate(idx).tolist())
